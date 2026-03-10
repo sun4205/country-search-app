83b0417435c72d8f2b13c79d583ddf08e1ab4036
sun4205 <hanna4205@gmail.com>
2026-03-10 13:17:46 -0400

    create country search app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const fetchCountry = async () => {
+    if (!query) return;
+
+    const res = await fetch(`https://restcountries.com/v3.1/name/${query}`);
+    const data = await res.json();
+
+    setCountries(data);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <p className="title">Country Search App</p>
+      <input
+        type="text"
+        placeholder="Please enter Country"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button className="button" onClick={fetchCountry}>
+        search
+      </button>
+      <div className="page">
+        {Countries.map((country) => (
+          <div key={country.cca3}>
+            <h3>{country.name.common}</h3>
+            <p>Capital:{country.capital?.[0]}</p>
+            <p>Population:{country.population}</p>
+            <img src={country.flags.png} width="100" />
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
